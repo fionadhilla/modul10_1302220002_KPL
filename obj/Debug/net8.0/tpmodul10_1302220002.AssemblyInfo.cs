32ec9436b624625141c01db6fd3f419987461aae
fionadhilla <fiona08dhilla@gmail.com>
2024-04-28 20:48:57 +0700

    first commit

--- obj/Debug/net8.0/tpmodul10_1302220002.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul10_1302220002.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul10_1302220002")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70424336df02a48528feb77ca7e43adec8bec637")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul10_1302220002")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul10_1302220002")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
